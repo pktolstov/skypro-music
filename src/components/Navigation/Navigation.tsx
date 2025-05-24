49e990a6547cd3c9304b0b01ad297a9c5908fc15
pktolstov <pktolstov@gmail.com>
2025-05-24 14:48:37 +0300

    Added components, styles, fonts

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './navigation.module.css';
+export default function Navigation() {
+  return (
+    <nav className={styles.main__nav}>
+      <div className={styles.nav__logo}>
+        <Image
+          width={113}
+          height={17}
+          className={'logo__image'}
+          src="/img/logo.png"
+          alt={'logo'}
+        />
+      </div>
+      <div className={styles.nav__burger}>
+        <span className={styles.burger__line}></span>
+        <span className={styles.burger__line}></span>
+        <span className={styles.burger__line}></span>
+      </div>
+      <div className={styles.nav__menu}>
+        <ul className={styles.menu__list}>
+          <li className={styles.menu__item}>
+            <Link href="#" className={styles.menu__link}>
+              Главное
+            </Link>
+          </li>
+          <li className={styles.menu__item}>
+            <Link href="#" className={styles.menu__link}>
+              Мой плейлист
+            </Link>
+          </li>
+          <li className={styles.menu__item}>
+            <Link href="../signin.html" className={styles.menu__link}>
+              Войти
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
