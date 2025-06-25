a959a1d362ec386775ff6f33f76e6ec952b3ca8f
pktolstov <pktolstov@gmail.com>
2025-06-25 21:45:11 +0300

    added function fetch and trackes

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,35 @@
+import styles from './signin.module.css';
+import classNames from 'classnames';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Signin() {
+  return (
+    <>
+      <a href="/music/main">
+        <div className={styles.modal__logo}>
+          <Image src="/img/logo_modal.png" alt="logo" width={140} height={21} />
+        </div>
+      </a>
+      <input
+        className={classNames(styles.modal__input, styles.login)}
+        type="text"
+        name="login"
+        placeholder="Почта"
+        autoComplete="username"
+      />
+      <input
+        className={classNames(styles.modal__input)}
+        type="password"
+        name="password"
+        placeholder="Пароль"
+        autoComplete="current-password"
+      />
+      <div className={styles.errorContainer}>{/*Блок для ошибок*/}</div>
+      <button className={styles.modal__btnEnter}>Войти</button>
+      <Link href={'/auth/signup'} className={styles.modal__btnSignup}>
+        Зарегистрироваться
+      </Link>
+    </>
+  );
+}
