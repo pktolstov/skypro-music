8785bf6a2dcbf94e88525432bb87954a292c7486
pktolstov <pktolstov@gmail.com>
2025-06-30 09:10:57 +0300

    Improved page.tsx Categories

--- src/app/music/category/[id]/page.tsx
+++ src/app/music/category/[id]/page.tsx
@@ -6,46 +6,47 @@ import { useState, useEffect } from 'react';
 import { getTracks, getTrackSet } from '@/services/tracksApi';
 import { TrackType, TrackSetType } from '@/sharedTypes/sharedTypes';
 import { AxiosError } from 'axios';
+import { useAppSelector } from '@/store/store';
 import styles from '../../layout.module.css';
 
 export default function CategoryPage() {
   const params = useParams<{ id: string }>();
-  const [tracks, setTracks] = useState<TrackType[]>([]);
+  const { allTracks, fetchIsLoading } = useAppSelector((state) => state.tracks);
+  const [isLoading, setIsLoading] = useState(false);
   const [filteredTracks, setFilteredTracks] = useState<TrackType[]>([]);
   const [trackSet, setTrackSet] = useState<TrackSetType | null>(null);
   const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const allTracks = await getTracks();
-        const trackSetResponse = await getTrackSet(params.id);
+      if (!fetchIsLoading) {
+        try {
+          const trackSetResponse = await getTrackSet(params.id);
 
-        // console.log('trackSet:', trackSetResponse);
+          // console.log('trackSet:', trackSetResponse);
 
-        const filtered = getTracksByIds(allTracks, trackSetResponse.items);
+          const filtered = getTracksByIds(allTracks, trackSetResponse.items);
 
-        setTracks(allTracks);
-        setFilteredTracks(filtered);
-        setTrackSet(trackSetResponse);
-      } catch (error) {
-        if (error instanceof AxiosError) {
-          if (error.response) {
-            setError(error.response.data);
-            console.log(error.response.data);
-          } else if (error.request) {
-            setError(error.request.data);
-            console.log(error.request);
-          } else {
-            setError(`Error, ${error.message}`);
-            console.log('Error', error.message);
+          setFilteredTracks(filtered);
+          setTrackSet(trackSetResponse);
+        } catch (error) {
+          if (error instanceof AxiosError) {
+            if (error.response) {
+              setError(error.response.data);
+            } else if (error.request) {
+              setError(error.request.data);
+            } else {
+              setError(`Error, ${error.message}`);
+            }
           }
+        } finally {
+          setIsLoading(false);
         }
       }
     };
 
     fetchData();
-  }, [params.id]);
+  }, [fetchIsLoading]);
 
   if (error) {
     return;
@@ -55,68 +56,11 @@ export default function CategoryPage() {
   }
 
   return (
-    <Centerblock 
-    // data={allTracks}
-    // isLoading={fetchIsLoading}
-    // errorRes={fetchError}
-    // title={'Треки'}
-    
-    data={filteredTracks} title={trackSet ? trackSet.name : ''} />
+    <Centerblock
+      isLoading={fetchIsLoading}
+      errorRes={error}
+      data={filteredTracks}
+      title={trackSet ? trackSet.name : ''}
+    />
   );
 }
-
-// 'use client';
-// import Centerblock from '@/components/Centerblock/Centerblock';
-
-// import { useParams } from 'next/navigation';
-// import { getTracksByIds } from '@/utils/helper';
-// import { useState } from 'react';
-// import { getTracks, getTrackSet } from '@/services/tracksApi';
-// import { TrackType,TrackSetType } from '@/sharedTypes/sharedTypes';
-// import { useEffect } from 'react';
-// import { AxiosError } from 'axios';
-
-// export default function CategoryPage() {
-//   const params = useParams<{ id: string }>();
-//   const [tracks, setTracks] = useState<TrackType[]>([]);
-//   const [tracksSetId, setTracksId] = useState<TrackSetType>();
-//   const [error, setError] = useState('');
-//   useEffect(() => {
-//     getTracks()
-//       .then((res) => {
-//         getTrackSet(params.id)
-//         .then((response) => {
-
-//           console.log(response);
-//           setTracksId(response.items)
-//           console.log(tracksSetId);
-
-//           // setTracks(getTracksByIds(tracks, tracksSetId))
-
-//         })
-//         setTracks(res);
-//       })
-//       .catch((error) => {
-//         if (error instanceof AxiosError) {
-//           if (error.response) {
-//             setError(error.response.data);
-//             // The request was made and the server responded with a status code
-//             // that falls out of the range of 2xx
-//             console.log(error.response.data);
-//             console.log(error.response.status);
-//             console.log(error.response.headers);
-//           } else if (error.request) {
-//             // The request was made but no response was received
-//             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-//             // http.ClientRequest in node.js
-//             console.log(error.request);
-//           } else {
-//             // Something happened in setting up the request that triggered an Error
-//             setError(`Error, ${error.message}`);
-//             console.log('Error', error.message);
-//           }
-//         }
-//       });
-//   }, []);
-//   return <Centerblock data={tracks} title="Треки" />;
-// }
